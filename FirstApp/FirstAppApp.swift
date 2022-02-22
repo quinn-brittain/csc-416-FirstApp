f3dbf94ac08c45f62f328e2188b1a84348ef0440
Quinn Brittain <qgbritt@gmail.com>
2022-02-22 14:57:21 -0500

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Quinn Brittain on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
