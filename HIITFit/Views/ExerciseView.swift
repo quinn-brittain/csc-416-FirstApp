6a7b5e751bf4825b72b05b13a8166714e610eec4
Quinn Brittain <qgbritt@gmail.com>
2022-03-08 00:44:05 -0500

    Add VideoPlayer

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -34,19 +34,30 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ExerciseView: View {
     let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
     let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
     let index: Int
     var body: some View {
-        VStack {
-            HeaderView(exerciseName: exerciseNames[index])
-            Text("Video player")
-            Text("Timer")
-            Text("Start/Done button")
-            Text("Rating")
-            Text("History button")
+        GeometryReader { geometry in
+            VStack {
+                HeaderView(exerciseName: exerciseNames[index])
+                    .padding(.bottom)
+                if let url = Bundle.main.url(forResource: videoNames[index], withExtension: "mp4") {
+                    VideoPlayer(player: AVPlayer(url: url))
+                        .frame(height: geometry.size.height * 0.5)
+                } else {
+                    Text("Couldn’t find \(videoNames[index]).mp4")
+                        .foregroundColor(.red)
+
+                }
+                Text("Timer")
+                Text("Start/Done button")
+                Text("Rating")
+                Text("History button")
+            }
         }
     }
 }
