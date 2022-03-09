5994a4ee69b77e057e53f7045b71ba864aba2cc7
Quinn Brittain <qgbritt@gmail.com>
2022-03-08 22:16:52 -0500

    Add Image to WelcomeView

--- HIITFit/Views/WelcomeView.swift
+++ HIITFit/Views/WelcomeView.swift
@@ -45,13 +45,18 @@ struct WelcomeView: View {
                     .padding(.bottom)
             }
             VStack {
-                HStack {
+                HStack(alignment: .bottom) {
                     VStack(alignment: .leading) {
                         Text("Get fit")
                             .font(.largeTitle)
                         Text("with high intensity interval training")
                             .font(.headline)
                     }
+                    Image("step-up")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 240.0, height: 240.0)
+                        .clipShape(Circle())
                 }
             }
         }
