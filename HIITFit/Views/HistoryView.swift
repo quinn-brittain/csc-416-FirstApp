f1eb963bfa4dfbdc906bb995618c5f3f5877d574
Quinn Brittain <qgbritt@gmail.com>
2022-03-08 21:46:26 -0500

    Add HistoryView Content

--- HIITFit/Views/HistoryView.swift
+++ HIITFit/Views/HistoryView.swift
@@ -41,9 +41,23 @@ struct HistoryView: View {
 
     var body: some View {
         VStack {
-          Text("History")
-            .font(.title)
-            .padding()
+            Text("History")
+                .font(.title)
+                .padding()
+            Form {
+                Section(header: Text(today.formatted(as: "MMM d"))
+                            .font(.headline)) {
+                    ForEach(exercises1, id: \.self) { exercise in
+                        Text(exercise)
+                    }
+                }
+                Section(header: Text(yesterday.formatted(as: "MMM d"))
+                            .font(.headline)) {
+                    ForEach(exercises2, id: \.self) { exercise in
+                        Text(exercise)
+                    }
+                }
+            }
         }
     }
 }
