bbcd0562653ade63084cca14db92bdff0213a9e4
Quinn Brittain <qgbritt@gmail.com>
2022-03-08 21:51:22 -0500

    Add HistoryView Close Button

--- HIITFit/Views/HistoryView.swift
+++ HIITFit/Views/HistoryView.swift
@@ -40,21 +40,28 @@ struct HistoryView: View {
     let exercises2 = ["Squat", "Step Up", "Burpee"]
 
     var body: some View {
-        VStack {
-            Text("History")
-                .font(.title)
-                .padding()
-            Form {
-                Section(header: Text(today.formatted(as: "MMM d"))
-                            .font(.headline)) {
-                    ForEach(exercises1, id: \.self) { exercise in
-                        Text(exercise)
+        ZStack(alignment: .topTrailing) {
+            Button(action: {}) {
+              Image(systemName: "xmark.circle")
+            }
+            .font(.title)
+            .padding(.trailing)
+            VStack {
+                Text("History")
+                    .font(.title)
+                    .padding()
+                Form {
+                    Section(header: Text(today.formatted(as: "MMM d"))
+                                .font(.headline)) {
+                        ForEach(exercises1, id: \.self) { exercise in
+                            Text(exercise)
+                        }
                     }
-                }
-                Section(header: Text(yesterday.formatted(as: "MMM d"))
-                            .font(.headline)) {
-                    ForEach(exercises2, id: \.self) { exercise in
-                        Text(exercise)
+                    Section(header: Text(yesterday.formatted(as: "MMM d"))
+                                .font(.headline)) {
+                        ForEach(exercises2, id: \.self) { exercise in
+                            Text(exercise)
+                        }
                     }
                 }
             }
