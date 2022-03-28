61b30566040408bda507245d7ee238a755c8b83e
Quinn Brittain <qgbritt@gmail.com>
2022-03-28 00:17:19 -0400

    Improving timer

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -39,13 +39,14 @@ import AVKit
 struct ExerciseView: View {
     @Binding var selectedTab: Int
     let index: Int
-    
-    let interval: TimeInterval = 30
+
+    @State private var timerDone = false
+    @State private var showTimer = false
+    @State private var rating = 0
+    @State private var showHistory = false
     var lastExercise: Bool {
         index + 1 == Exercise.exercises.count
     }
-    @State private var rating = 0
-    @State private var showHistory = false
     @State private var showSuccess = false
     
     var body: some View {
@@ -63,26 +64,33 @@ struct ExerciseView: View {
                     Text("Couldn’t find \(Exercise.exercises[index].videoName).mp4")
                         .foregroundColor(.red)
                 }
-                Text(Date().addingTimeInterval(interval), style: .timer)
-                    .font(.system(size: 90))
                 HStack(spacing: 150) {
-                    Button("Start Exercise") { }
+                    Button("Start Exercise") {
+                        showTimer.toggle()
+                    }
                     Button("Done") {
+                        timerDone = false
+                        showTimer.toggle()
+
                         if lastExercise {
                             showSuccess.toggle()
                         } else {
                             selectedTab += 1
                         }
                     }
+                    .disabled(!timerDone)
                     .sheet(isPresented: $showSuccess) {
                         SuccessView(selectedTab: $selectedTab)
                     }
                 }
                 .font(.title3)
                 .padding()
+                if showTimer {
+                    TimerView(timerDone: $timerDone)
+                }
+                Spacer()
                 RatingView(rating: $rating)
                     .padding()
-                Spacer()
                 Button("History") {
                     showHistory.toggle()
                 }
