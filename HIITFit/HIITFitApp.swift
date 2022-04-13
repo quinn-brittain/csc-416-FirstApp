7a7fed6b529ad5b89b9f9cc5e5f8151e9c9e3848
Quinn Brittain <qgbritt@gmail.com>
2022-04-12 22:30:17 -0400

    Add history error alerts

--- HIITFit/HIITFitApp.swift
+++ HIITFit/HIITFitApp.swift
@@ -36,13 +36,15 @@ import SwiftUI
 @main
 struct HIITFitApp: App {
     @StateObject private var historyStore: HistoryStore
+    @State private var showAlert = false
 
     init() {
         let historyStore: HistoryStore
         do {
             historyStore = try HistoryStore(withChecking: true)
         } catch {
-            print("Could not load history data")
+            print(">>> Error: Could not load history data")
+            showAlert = true
             historyStore = HistoryStore()
         }
         _historyStore = StateObject(wrappedValue: historyStore)
@@ -52,6 +54,16 @@ struct HIITFitApp: App {
         WindowGroup {
             ContentView()
                 .environmentObject(historyStore)
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("History"),
+                        message: Text(
+                      """
+                      Unfortunately we can’t load your past history.
+                      Email support:
+                        support@xyz.com
+                      """))
+                }
         }
     }
 }
