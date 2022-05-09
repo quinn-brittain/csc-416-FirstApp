56edaac5b0dc2dbf5173943d25abab21871bfed8
Quinn Brittain <qgbritt@gmail.com>
2022-05-09 02:34:45 -0400

    Change header page marker style

--- HIITFit/Views/HeaderView.swift
+++ HIITFit/Views/HeaderView.swift
@@ -33,36 +33,44 @@
 import SwiftUI
 
 struct HeaderView: View {
-  @Binding var selectedTab: Int
-  let titleText: String
-
-  var body: some View {
-    VStack {
-      Text(titleText)
-        .font(.largeTitle)
-      HStack {
-        ForEach(0 ..< Exercise.exercises.count) { index in
-          let fill = index == selectedTab ? ".fill" : ""
-          Image(systemName: "\(index + 1).circle\(fill)")
-            .onTapGesture {
-              selectedTab = index
+    @Binding var selectedTab: Int
+    let titleText: String
+    
+    var body: some View {
+        VStack {
+            Text(titleText)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.white)
+            HStack {
+                ForEach(0 ..< Exercise.exercises.count) { index in
+                    ZStack {
+                        Circle()
+                            .frame(width: 32, height: 32)
+                            .foregroundColor(.white)
+                            .opacity(index == selectedTab ? 0.5 : 0)
+                        Circle()
+                            .frame(width: 16, height: 16)
+                            .foregroundColor(.white)
+                    }
+                    .onTapGesture {
+                        selectedTab = index
+                    }
+                }
             }
         }
-      }
-      .font(.title2)
     }
-  }
 }
 
 struct HeaderView_Previews: PreviewProvider {
-  static var previews: some View {
-    Group {
-      HeaderView(selectedTab: .constant(0), titleText: "Squat")
-        .previewLayout(.sizeThatFits)
-      HeaderView(selectedTab: .constant(1), titleText: "Step Up")
-        .preferredColorScheme(.dark)
-        .environment(\.sizeCategory, .accessibilityLarge)
-        .previewLayout(.sizeThatFits)
+    static var previews: some View {
+        Group {
+            HeaderView(selectedTab: .constant(0), titleText: "Squat")
+                .previewLayout(.sizeThatFits)
+            HeaderView(selectedTab: .constant(1), titleText: "Step Up")
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .accessibilityLarge)
+                .previewLayout(.sizeThatFits)
+        }
     }
-  }
 }
