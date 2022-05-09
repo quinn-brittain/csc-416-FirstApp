8952674442470dfb6ec5e1b953ebbc271b56f325
Quinn Brittain <qgbritt@gmail.com>
2022-05-09 01:35:46 -0400

    Add RaisedButtonStyle to  to welcome view

--- HIITFit/Views/WelcomeView.swift
+++ HIITFit/Views/WelcomeView.swift
@@ -33,51 +33,48 @@
 import SwiftUI
 
 struct WelcomeView: View {
-  @State private var showHistory = false
-  @Binding var selectedTab: Int
+    @State private var showHistory = false
+    @Binding var selectedTab: Int
 
-  var body: some View {
-    ZStack {
-      VStack {
-        HeaderView(selectedTab: $selectedTab, titleText: "Welcome")
-        Spacer()
-        Button("History") {
-          showHistory.toggle()
+    var body: some View {
+        ZStack {
+            VStack {
+                HeaderView(selectedTab: $selectedTab, titleText: "Welcome")
+                Spacer()
+                Button("History") {
+                    showHistory.toggle()
+                }
+                .sheet(isPresented: $showHistory) {
+                    HistoryView(showHistory: $showHistory)
+                }
+                .padding(.bottom)
+            }
+            VStack {
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("Get fit")
+                            .font(.largeTitle)
+                        Text("with high intensity interval training")
+                            .font(.headline)
+                    }
+                    Image("step-up")
+                        .resizedToFill(width: 240, height: 240)
+                        .clipShape(Circle())
+                }
+                // swiftlint:disable:next multiple_closures_with_trailing_closure
+                Button(action: { selectedTab = 0 }) {
+                    Text("Get Started")
+                        .raisedButtonTextStyle()
+                }
+                .buttonStyle(RaisedButtonStyle())
+                .padding()
+            }
         }
-        .sheet(isPresented: $showHistory) {
-          HistoryView(showHistory: $showHistory)
-        }
-        .padding(.bottom)
-      }
-      VStack {
-        HStack(alignment: .bottom) {
-          VStack(alignment: .leading) {
-            Text("Get fit")
-              .font(.largeTitle)
-            Text("with high intensity interval training")
-              .font(.headline)
-          }
-          Image("step-up")
-            .resizedToFill(width: 240, height: 240)
-            .clipShape(Circle())
-        }
-        // swiftlint:disable:next multiple_closures_with_trailing_closure
-        Button(action: { selectedTab = 0 }) {
-          Text("Get Started")
-          Image(systemName: "arrow.right.circle")
-        }
-        .font(.title2)
-        .padding()
-        .background(
-          RoundedRectangle(cornerRadius: 20)
-            .stroke(Color.gray, lineWidth: 2))
-      }
     }
-  }
 }
 
 struct WelcomeView_Previews: PreviewProvider {
-  static var previews: some View {
-    WelcomeView(selectedTab: .constant(9))
-  }
+    static var previews: some View {
+        WelcomeView(selectedTab: .constant(9))
+    }
 }
