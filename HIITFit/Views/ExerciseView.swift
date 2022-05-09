163bad8f8664f90c1321e386efe28f658146044e
Quinn Brittain <qgbritt@gmail.com>
2022-05-09 02:54:18 -0400

    Fix exercise view

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -37,10 +37,17 @@ struct ExerciseView: View {
     @EnvironmentObject var history: HistoryStore
     @State private var showHistory = false
     @State private var showSuccess = false
+    @State private var showSheet = false
     @Binding var selectedTab: Int
     let index: Int
     @State private var timerDone = false
     @State private var showTimer = false
+
+    @State private var exerciseSheet: ExerciseSheet?
+
+    enum ExerciseSheet {
+        case history, timer, success
+    }
 
     var lastExercise: Bool {
         index + 1 == Exercise.exercises.count
@@ -49,6 +56,22 @@ struct ExerciseView: View {
     var startExerciseButton: some View {
         RaisedButton(buttonText: "Start Exercise") {
             showTimer.toggle()
+        }
+    }
+
+    @ViewBuilder
+    func video(size: CGSize) -> some View {
+        if let url = Bundle.main.url(
+            forResource: Exercise.exercises[index].videoName,
+            withExtension: "mp4") {
+            VideoPlayer(player: AVPlayer(url: url))
+                .frame(height: size.height * 0.25)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .padding(20)
+        } else {
+            Text(
+                "Couldn't find \(Exercise.exercises[index].videoName).mp4")
+                .foregroundColor(.red)
         }
     }
 
@@ -72,42 +95,54 @@ struct ExerciseView: View {
                     selectedTab: $selectedTab,
                     titleText: Exercise.exercises[index].exerciseName)
                     .padding(.bottom)
-                if let url = Bundle.main.url(
-                    forResource: Exercise.exercises[index].videoName,
-                    withExtension: "mp4") {
-                    VideoPlayer(player: AVPlayer(url: url))
-                        .frame(height: geometry.size.height * 0.45)
-                } else {
-                    Text(
-                        "Couldn't find \(Exercise.exercises[index].videoName).mp4")
-                        .foregroundColor(.red)
+                Spacer()
+                ContainerView {
+                    VStack {
+                        video(size: geometry.size)
+                        startExerciseButton
+                            .padding(20)
+                        RatingView(exerciseIndex: index)
+                            .padding()
+                        Spacer()
+                        historyButton
+                    }
                 }
-                HStack(spacing: 150) {
-                    startExerciseButton
-                    Button("Done") {
-                        history.addDoneExercise(Exercise.exercises[index].exerciseName)
-                        timerDone = false
-                        showTimer.toggle()
+                .frame(height: geometry.size.height * 0.8)
+                .sheet(isPresented: $showSheet, onDismiss: {
+                    showSuccess = false
+                    showHistory = false
+                    if exerciseSheet == .timer {
+                        if timerDone {
+                            history.addDoneExercise(Exercise.exercises[index].exerciseName)
+                            timerDone = false
+                        }
+                        showTimer = false
                         if lastExercise {
-                            showSuccess.toggle()
+                            showSuccess = true
+                            showSheet = true
+                            exerciseSheet = .success
                         } else {
                             selectedTab += 1
                         }
+                    } else {
+                        exerciseSheet = nil
                     }
-                    .disabled(!timerDone)
-                    .sheet(isPresented: $showSuccess) {
-                        SuccessView(selectedTab: $selectedTab)
+                    showTimer = false
+                }, content: {
+                    if let exerciseSheet = exerciseSheet {
+                        switch exerciseSheet {
+                        case .history:
+                            HistoryView(showHistory: $showHistory)
+                                .environmentObject(history)
+                        case .timer:
+                            TimerView(
+                                timerDone: $timerDone,
+                                exerciseName: Exercise.exercises[index].exerciseName)
+                        case .success:
+                            SuccessView(selectedTab: $selectedTab)
+                        }
                     }
-                }
-                .font(.title3)
-                .padding()
-                if showTimer {
-                    TimerView(timerDone: $timerDone)
-                }
-                Spacer()
-                RatingView(exerciseIndex: index)
-                    .padding()
-                historyButton
+                })
             }
         }
     }
--- HIITFit/Views/TimerView.swift
+++ HIITFit/Views/TimerView.swift
@@ -33,31 +33,87 @@
 import SwiftUI
 
 struct TimerView: View {
-  @State private var timeRemaining = 3 // 30
-  @Binding var timerDone: Bool
-  let timer = Timer.publish(
-    every: 1,
-    on: .main,
-    in: .common)
-    .autoconnect()
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var timerDone: Bool
+    @State private var timeRemaining = 3 // 30
+    let exerciseName: String
+    let timer = Timer.publish(
+        every: 1,
+        on: .main,
+        in: .common)
+        .autoconnect()
 
-  var body: some View {
-    Text("\(timeRemaining)")
-      .font(.system(size: 90, design: .rounded))
-      .padding()
-      .onReceive(timer) { _ in
-        if self.timeRemaining > 0 {
-          self.timeRemaining -= 1
-        } else {
-          timerDone = true
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color("background")
+                    .edgesIgnoringSafeArea(.all)
+                circle(size: geometry.size)
+                    .overlay(
+                        GradientBackground()
+                            .mask(circle(size: geometry.size))
+                    )
+                VStack {
+                    Text(exerciseName)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(.white)
+                        .padding(.top, 20)
+                    Spacer()
+                    IndentView {
+                        timerText
+                    }
+                    Spacer()
+                    RaisedButton(buttonText: "Done") {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    .opacity(timerDone ? 1 : 0)
+                    .padding([.leading, .trailing], 30)
+                    .padding(.bottom, 60)
+                    .disabled(!timerDone)
+                }
+                .onAppear {
+                    timerDone = false
+                }
+            }
         }
-      }
-  }
+    }
+
+    var timerText: some View {
+        Text("\(timeRemaining)")
+            .font(.system(size: 90, design: .rounded))
+            .fontWeight(.heavy)
+            .frame(
+                minWidth: 180,
+                maxWidth: 200,
+                minHeight: 180,
+                maxHeight: 200)
+            .padding()
+            .onReceive(timer) { _ in
+                if self.timeRemaining > 0 {
+                    self.timeRemaining -= 1
+                } else {
+                    timerDone = true
+                }
+            }
+    }
+
+    func circle(size: CGSize) -> some View {
+        Circle()
+            .frame(
+                width: size.width,
+                height: size.height)
+            .position(
+                x: size.width * 0.5,
+                y: -size.width * 0.2)
+    }
 }
 
 struct TimerView_Previews: PreviewProvider {
-  static var previews: some View {
-    TimerView(timerDone: .constant(false))
-      .previewLayout(.sizeThatFits)
-  }
+    static var previews: some View {
+        TimerView(
+            timerDone: .constant(false),
+            exerciseName: "Step Up")
+    }
 }
